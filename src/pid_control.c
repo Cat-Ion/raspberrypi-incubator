8451b620485a97ab5fbbe1c336862894310b9c37
Valentin Ochs <a@0au.de>
2013-09-05 14:47:00 +0200

    [feature/comments] Comments for pid_control.c

--- src/pid_control.c
+++ src/pid_control.c
@@ -8,6 +8,7 @@ struct pid_t {
 	float acc;
 } pid_temp, pid_hum;
 
+/* Initialize the PID values to the defaults */
 void pid_init() {
 	pid_temp.p   = T_P;
 	pid_temp.i   = T_I;
@@ -22,6 +23,8 @@ void pid_init() {
 	pid_hum.acc = 0;
 }
 
+/* Writes the current temp/humidity PID values to the given
+   pointers */
 void pid_getvalues(float *tp, float *ti, float *td,
                    float *hp, float *hi, float *hd) {
 	*tp = pid_temp.p;
@@ -32,6 +35,7 @@ void pid_getvalues(float *tp, float *ti, float *td,
 	*hd = pid_hum.d;
 }
 
+/* Sets the current temp/humidity PID values from the given floats */
 void pid_setvalues(float tp, float ti, float td,
                    float hp, float hi, float hd) {
 	pid_temp.p = tp;
@@ -42,6 +46,8 @@ void pid_setvalues(float tp, float ti, float td,
 	pid_hum.d  = hd;
 }
 
+/* Returns the voltage to use for heating, as a fraction of the
+   maximum voltage, given the current temperature. */
 static float temp_control(float temp) {
 	float diff = temp - wanted_temperature;
 
@@ -63,6 +69,8 @@ static float temp_control(float temp) {
 	return r;
 }
 
+/* Control the heating and humidifier as a function of the current
+   environment values */
 void pid_control(float temp, float hum) {
 	set_voltage(temp_control(temp));
 }
